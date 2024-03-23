55c26feabab5f19c4e114c31fcf0b53087cfd76e
0Bao <1140066585@qq.com>
2024-03-23 16:27:46 +0800

    数学库添加矩阵变换
     - ignore build

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,12 +1,134 @@
 #include "Math.h"
 
+#include "Base.h"
+#include <math.h>
+
 namespace RGS {
+
+    float Dot(const Vec3& left, const Vec3& right)
+    {
+        return left.X * right.X + left.Y * right.Y + left.Z * right.Z;
+    }
+
+    Mat4::Mat4(const Vec4& v0, const Vec4& v1, const Vec4& v2, const Vec4& v3)
+    {
+        M[0][0] = v0.X; M[1][0] = v0.Y; M[2][0] = v0.Z; M[3][0] = v0.W;
+        M[0][1] = v1.X; M[1][1] = v1.Y; M[2][1] = v1.Z; M[3][1] = v1.W;
+        M[0][2] = v2.X; M[1][2] = v2.Y; M[2][2] = v2.Z; M[3][2] = v2.W;
+        M[0][3] = v3.X; M[1][3] = v3.Y; M[2][3] = v3.Z; M[3][3] = v3.W;
+    }
+
+    Vec4 operator* (const Mat4& mat4, const Vec4& vec4)
+    {
+        Vec4 res;
+        res.X = mat4.M[0][0] * vec4.X + mat4.M[0][1] * vec4.Y + mat4.M[0][2] * vec4.Z + mat4.M[0][3] * vec4.W;
+        res.Y = mat4.M[1][0] * vec4.X + mat4.M[1][1] * vec4.Y + mat4.M[1][2] * vec4.Z + mat4.M[1][3] * vec4.W;
+        res.Z = mat4.M[2][0] * vec4.X + mat4.M[2][1] * vec4.Y + mat4.M[2][2] * vec4.Z + mat4.M[2][3] * vec4.W;
+        res.W = mat4.M[3][0] * vec4.X + mat4.M[3][1] * vec4.Y + mat4.M[3][2] * vec4.Z + mat4.M[3][3] * vec4.W;
+        return res;
+    }
+    Mat4 operator* (const Mat4& left, const Mat4& right)
+    {
+        Mat4 res;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    res.M[i][j] += left.M[i][k] * right.M[k][j];
+                }
+            }
+        }
+        return res;
+    }
+    Mat4& operator*= (Mat4& left, const Mat4& right)
+    {
+        left = left * right;
+        return left;
+    }
+
+    Mat4 Mat4Identity()
+    {
+        return Mat4({ 1,0,0,0 }, { 0,1,0,0 }, { 0,0,1,0 }, { 0,0,0,1 });
+    }
+    Mat4 Mat4Translate(float tx, float ty, float tz)
+    {
+        Mat4 m = Mat4Identity();
+        m.M[0][3] = tx;
+        m.M[1][3] = ty;
+        m.M[2][3] = tz;
+        return m;
+    }
+    Mat4 Mat4Scale(float sx, float sy, float sz)
+    {
+        Mat4 m = Mat4Identity();
+        ASSERT(sx != 0 && sy != 0 && sz != 0);
+        m.M[0][0] = sx;
+        m.M[1][1] = sy;
+        m.M[2][2] = sz;
+        return m;
+    }
+    Mat4 Mat4RotateX(float angle)
+    {
+        float c = (float)cos(angle);
+        float s = (float)sin(angle);
+        Mat4 m = Mat4Identity();
+        m.M[1][1] = c;
+        m.M[1][2] = -s;
+        m.M[2][1] = s;
+        m.M[2][2] = c;
+        return m;
+    }
+    Mat4 Mat4RotateY(float angle)
+    {
+        float c = (float)cos(angle);
+        float s = (float)sin(angle);
+        Mat4 m = Mat4Identity();
+        m.M[2][2] = c;
+        m.M[2][0] = -s;
+        m.M[0][2] = s;
+        m.M[0][0] = c;
+        return m;
+    }
+    Mat4 Mat4RotateZ(float angle)
+    {
+        float c = (float)cos(angle);
+        float s = (float)sin(angle);
+        Mat4 m = Mat4Identity();
+        m.M[0][0] = c;
+        m.M[0][1] = -s;
+        m.M[1][0] = s;
+        m.M[1][1] = c;
+        return m;
+    }
+    Mat4 Mat4LookAt(const Vec3& xAxis, const Vec3& yAxis, const Vec3& zAxis, const Vec3& eye)
+    {
+        Mat4 m = Mat4Identity();
+
+        m.M[0][0] = xAxis.X;
+        m.M[0][1] = xAxis.Y;
+        m.M[0][2] = xAxis.Z;
+
+        m.M[1][0] = yAxis.X;
+        m.M[1][1] = yAxis.Y;
+        m.M[1][2] = yAxis.Z;
+
+        m.M[2][0] = zAxis.X;
+        m.M[2][1] = zAxis.Y;
+        m.M[2][2] = zAxis.Z;
+
+        m.M[0][3] = -Dot(xAxis, eye);
+        m.M[1][3] = -Dot(yAxis, eye);
+        m.M[2][3] = -Dot(zAxis, eye);
+
+        return m;
+    }
 
     unsigned char Float2UChar(const float f)
     {
         return (unsigned char)(f * 255.0f);
     }
-
     float UChar2Float(const unsigned char c)
     {
         return (float)c / 255.0f;
--- src/Math.h
+++ src/Math.h
@@ -1,19 +1,112 @@
-#pragma once 
+#pragma once
+#include <string>
 
 namespace RGS {
+
+    struct Vec2
+    {
+        float X, Y;
+
+        constexpr Vec2()
+            : X(0.0f), Y(0.0f) {}
+        constexpr Vec2(float x, float y)
+            : X(x), Y(y) {}
+    };
+
 	struct Vec3
 	{
-		float X;
-		float Y;
-		float Z;
+        float X, Y, Z;
 
 		// Learn constexpr: https://learn.microsoft.com/zh-cn/cpp/cpp/constexpr-cpp?view=msvc-170
 		constexpr Vec3()
 			: X(0.0f), Y(0.0f), Z(0.0f) {}
 		constexpr Vec3(float x, float y, float z)
 			: X(x), Y(y), Z(z) {}
+
+        operator Vec2() const { return { X, Y }; }
 	};
 
+    struct Vec4 
+    {
+        float X ,Y, Z, W;
+
+        constexpr Vec4()
+            : X(0.0f), Y(0.0f), Z(0.0f), W(0.0f) {}
+        constexpr Vec4(float x, float y, float z, float w)
+            : X(x), Y(y), Z(z), W(w) {}
+        constexpr Vec4(const Vec3& vec3, float w)
+            : X(vec3.X), Y(vec3.Y), Z(vec3.Z), W(w) {}
+
+        operator Vec2() const { return { X, Y }; }
+        operator Vec3() const { return { X, Y, Z }; }
+
+        operator std::string() const
+        {
+            std::string res;
+            res += "(";
+            res += std::to_string(X);
+            res += ", ";
+            res += std::to_string(Y);
+            res += ", ";
+            res += std::to_string(Z);
+            res += ", ";
+            res += std::to_string(W);
+            res += ")";
+            return res;
+        }
+
+    };
+
+    // 按行优先存储，第一列向量为 [0][0] [1][0] [2][0] [3][0] <=> 0, 4, 8, 12
+    struct Mat4
+    {
+        float M[4][4];
+
+        Mat4()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    M[i][j] = 0.0f;
+                }
+            }
+
+        }
+        // 按列存入
+        Mat4(const Vec4& v0, const Vec4& v1, const Vec4& v2, const Vec4& v3);
+
+        operator const std::string() const
+        {
+            std::string res;
+            res += "(";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    res += std::to_string(M[i][j]);
+                    res += (i == 3 && j == 3) ? " )" : ", ";
+                }
+            }
+            return res;
+        }
+    };
+
+    float Dot(const Vec3& left, const Vec3& right);
+
+    Vec4 operator* (const Mat4& mat4, const Vec4& vec4);
+    Mat4 operator* (const Mat4& left, const Mat4& right);
+    Mat4& operator*= (Mat4& left, const Mat4& right);
+
+    Mat4 Mat4Identity();
+    Mat4 Mat4Translate(float tx, float ty, float tz);
+    Mat4 Mat4Scale(float sx, float sy, float sz);
+    Mat4 Mat4RotateX(float angle);
+    Mat4 Mat4RotateY(float angle);
+    Mat4 Mat4RotateZ(float angle);
+    Mat4 Mat4LookAt(const Vec3& xAxis, const Vec3& yAxis, const Vec3& zAxis, const Vec3& eye);
+    
 	unsigned char Float2UChar(const float f);
 	float UChar2Float(const unsigned char c);
 }
